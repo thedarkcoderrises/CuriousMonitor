a10146bbc381bc7bab6f4c681403ec2d098da58a
thedarkcoderrises <abhikrish88@gmail.com>
2019-05-25 11:22:40 +0530

    Image Container Chart

--- src/main/java/com/tdcr/docker/ui/views/dashboard/DashboardView.java
+++ src/main/java/com/tdcr/docker/ui/views/dashboard/DashboardView.java
@@ -1,7 +1,9 @@
 package com.tdcr.docker.ui.views.dashboard;
 
 import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.api.model.Statistics;
 import com.tdcr.docker.app.HasLogger;
+import com.tdcr.docker.backend.data.entity.DockContainer;
 import com.tdcr.docker.backend.data.entity.DockImage;
 import com.tdcr.docker.backend.data.entity.ImageDetails;
 import com.tdcr.docker.backend.data.entity.Incident;
@@ -123,8 +125,7 @@ public class DashboardView extends PolymerTemplate<TemplateModel> implements Has
 
 		final Configuration conf = imageContainerChart.getConfiguration();
 
-		conf.setTitle("Global happiness index");
-		conf.setSubTitle("Source: www.happyplanetindex.org");
+		conf.setTitle("Image Container details");
 		conf.getLegend().setEnabled(false);
 
 		XAxis x = new XAxis();
@@ -132,7 +133,7 @@ public class DashboardView extends PolymerTemplate<TemplateModel> implements Has
 		conf.addxAxis(x);
 
 		YAxis y = new YAxis();
-		y.setTitle("Total percent market share");
+		y.setTitle("Number of Containers");
 		conf.addyAxis(y);
 
 		PlotOptionsColumn column = new PlotOptionsColumn();
@@ -141,223 +142,53 @@ public class DashboardView extends PolymerTemplate<TemplateModel> implements Has
 
 		conf.setPlotOptions(column);
 
-		DataSeries regionsSeries = new DataSeries();
-		regionsSeries.setName("Regions");
+		DataSeries regionsSeries = new DataSeries("Images");
+		DataSeries countriesSeries = new DataSeries("Containers");
+
+
 		PlotOptionsColumn plotOptionsColumn = new PlotOptionsColumn();
 		plotOptionsColumn.setColorByPoint(true);
 		regionsSeries.setPlotOptions(plotOptionsColumn);
 
-		DataSeriesItem regionItem = new DataSeriesItem(
-				"Latin America and Carribean", 60);
-		DataSeries countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("Latin America and Carribean Countries");
+//		String dockerDeamonValue = searchBar.getComboBox().getValue();
+		String dockerDeamonValue = "LOCAL_DD";
+		DataSeriesItem regionItem;
+		DataSeriesItem countryItem;
+		DataSeries detailsSeries;
+		String[] categories = new String[]{"NullPointer Exception",
+				"DB Exception", "Others"};
+		Number[] ys;
 
-		DataSeriesItem countryItem = new DataSeriesItem("Costa Rica", 64);
-		DataSeries detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Costa Rica");
-		String[] categories = new String[] { "Life Expectancy",
-				"Well-being (0-10)", "Footprint (gha/capita)" };
-		Number[] ys = new Number[] { 79.3, 7.3, 2.5 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
 
-		countryItem = new DataSeriesItem("Colombia", 59.8);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Colombia");
-		ys = new Number[] { 73.7, 6.4, 1.8 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
+		if(dockerDeamonValue!=null) {
+			List<DockImage> dockImageList = dockerService.listAllImages(dockerDeamonValue);
 
-		countryItem = new DataSeriesItem("Belize", 59.3);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Belize");
-		ys = new Number[] { 76.1, 6.5, 2.1 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
+			for (DockImage dockImage : dockImageList) {
 
-		countryItem = new DataSeriesItem("El Salvador", 58.9);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details El Salvador");
-		ys = new Number[] { 72.2, 6.7, 2.0 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
+				countriesSeries.setId(dockImage.getImageName() + " Containers");
+				List<DockContainer> dockContainerList = dockerService.listAllContainers("running");
+				int nContainers = 0;
+				for (DockContainer dockContainer : dockContainerList) {
+					if ((dockContainer.getImageName()).equalsIgnoreCase(dockImage.getImageName().split(":")[0])) {
+						nContainers++;
 
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
+						Statistics stats = dockerService.getContainerRawStats(dockContainer.getContainerId());
+						int netWork = Integer.parseInt(String.valueOf(ComputeStats.calcNetworkStats(stats).split("/")[0].split("[a-zA-Z]")[0]));
+						countryItem = new DataSeriesItem(dockContainer.getContainerName(), netWork);
+						detailsSeries = new DataSeries("Details");
+						detailsSeries.setId(dockContainer.getContainerName() + " Details");
+						ys = new Number[]{70, 7, 15};
+						detailsSeries.setData(categories, ys);
+						countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
+						countriesSeries.add(countryItem);
+					}
+				}
 
-		regionItem = new DataSeriesItem("Western Nations", 50);
+				regionItem = new DataSeriesItem(dockImage.getImageName(), nContainers);
+				regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
+			}
+		}
 
-		countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("Western Nations Countries");
-
-		countryItem = new DataSeriesItem("New Zealand", 51.6);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details New Zealand");
-		ys = new Number[] { 80.7, 7.2, 4.3 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Norway", 51.4);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Norway");
-		ys = new Number[] { 81.1, 7.6, 4.8 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Switzerland", 50.3);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Switzerland");
-		ys = new Number[] { 82.3, 7.5, 5.0 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("United Kingdom", 47.9);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details United Kingdom");
-		ys = new Number[] { 80.2, 7.0, 4.7 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
-
-		regionItem = new DataSeriesItem("Middle East and North Africa", 53);
-
-		countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("Middle East and North Africa Countries");
-
-		countryItem = new DataSeriesItem("Israel", 55.2);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Israel");
-		ys = new Number[] { 81.6, 7.4, 4.0 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Algeria", 52.2);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Algeria");
-		ys = new Number[] { 73.1, 5.2, 1.6 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Jordan", 51.7);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Jordan");
-		ys = new Number[] { 73.4, 5.7, 2.1 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Palestine", 51.2);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Palestine");
-		ys = new Number[] { 72.8, 4.8, 1.4 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
-
-		regionItem = new DataSeriesItem("Sub-Saharan Africa", 42);
-
-		countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("Sub-Saharan Africa Countries");
-
-		countryItem = new DataSeriesItem("Nigeria", 51.6);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Nigeria");
-		ys = new Number[] { 66.7, 4.6, 1.2 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Malawi", 42.5);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Malawi");
-		ys = new Number[] { 54.2, 5.1, 0.8 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Ghana", 40.3);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Ghana");
-		ys = new Number[] { 64.2, 4.6, 1.7 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Ethiopia", 39.2);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Ethiopia");
-		ys = new Number[] { 59.3, 4.4, 1.1 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
-
-		regionItem = new DataSeriesItem("South Asia", 53);
-
-		countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("South Asia Countries");
-
-		countryItem = new DataSeriesItem("Bangladesh", 56.3);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Bangladesh");
-		ys = new Number[] { 68.9, 5.0, 0.7 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Pakistan", 54.1);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Pakistan");
-		ys = new Number[] { 65.4, 5.3, 0.8 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("India", 50.9);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details India");
-		ys = new Number[] { 65.4, 5.0, 0.9 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Sri Lanka", 51.2);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Sri Lanka");
-		ys = new Number[] { 74.9, 4.2, 1.2 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
-
-		regionItem = new DataSeriesItem("East Asia", 55);
-
-		countriesSeries = new DataSeries("Countries");
-		countriesSeries.setId("East Asia Countries");
-
-		countryItem = new DataSeriesItem("Vietnam", 60.4);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Vietnam");
-		ys = new Number[] { 75.2, 5.8, 1.4 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Indonesia", 55.5);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Indonesia");
-		ys = new Number[] { 69.4, 5.5, 1.1 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Thailand", 53.5);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Thailand");
-		ys = new Number[] { 74.1, 6.2, 2.4 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		countryItem = new DataSeriesItem("Philippines", 52.4);
-		detailsSeries = new DataSeries("Details");
-		detailsSeries.setId("Details Philippines");
-		ys = new Number[] { 68.7, 4.9, 1.0 };
-		detailsSeries.setData(categories, ys);
-		countriesSeries.addItemWithDrilldown(countryItem, detailsSeries);
-
-		regionsSeries.addItemWithDrilldown(regionItem, countriesSeries);
 
 		conf.addSeries(regionsSeries);
 	}
@@ -785,6 +616,7 @@ public class DashboardView extends PolymerTemplate<TemplateModel> implements Has
 				dockerService.updateDockerClient(e.getValue());
 				updateData(e.getValue());
 				searchBar.getActionButton().click();
+//				initImageContainerChart();
 			}
 		});
 	}
